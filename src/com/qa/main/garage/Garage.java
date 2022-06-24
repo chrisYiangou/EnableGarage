5fa25c17f2fc1dd35c9629b74397dee50ba8a58c
chrisYiangou <chrisyiangou100@gmail.com>
2022-06-24 10:50:34 +0100

    Added Final part of challenge

--- src/com/qa/main/garage/Garage.java
+++ src/com/qa/main/garage/Garage.java
@@ -13,6 +13,7 @@ public class Garage {
 	// Dependency Inversion
 	private List<Vehicle> garage;
 	private float totalBill;
+	private List<Vehicle> fixedVehicles;
 
 	public Garage() {
 		this.garage = new ArrayList<>();
@@ -28,14 +29,25 @@ public class Garage {
 	 * bill for each type of Vehicle in a different way, depending on the type of
 	 * Vehicle it is (this does not need to be complex).
 	 */
-	
+
+	// Fix vehicle
+
+	public void fixVehicle(int index) {
+		// Get the vehicle
+		fixedVehicles = new ArrayList();
+
+		fixedVehicles.add(garage.get(index));
+		calculateBill();
+		this.deleteVehicle(index);
+	}
+
 	public float calculateBill() {
 		for (int i = 0; i < garage.size(); i++) {
-			if (garage.get(i) instanceof Car) {
+			if (fixedVehicles.get(i) instanceof Car) {
 				totalBill += 50;
-			} else if (garage.get(i) instanceof MotorBike) {
+			} else if (fixedVehicles.get(i) instanceof MotorBike) {
 				totalBill += 25;
-			} else if (garage.get(i) instanceof Lorry) {
+			} else if (fixedVehicles.get(i) instanceof Lorry) {
 				totalBill += 100;
 			} else {
 				totalBill += 10;
@@ -66,5 +78,29 @@ public class Garage {
 	}
 
 	// Update, Delete,
+
+	// remove a Vehicle
+	// pop - Removes the the last instance AND returns the object
+	public void deleteVehicle(int index) {
+		garage.remove(index);
+	}
+
+	public void removeAll() {
+		garage.clear();
+	}
+
+	// Update
+
+	public void updateOne(int index, Vehicle vehicle) {
+
+		Vehicle previousVehicle = garage.get(index);
+
+		// Compare the two variables --
+		// PUT - It completely redoes everythng
+		// PATCH - changes certain elements
+
+		garage.set(index, vehicle);
+
+	}
 
 }
